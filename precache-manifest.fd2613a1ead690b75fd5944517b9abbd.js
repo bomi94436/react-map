890b2360ae5bd7808ab07ef70190918825a78617
bomi94436 <boo072963@naver.com>
2020-07-01 04:58:29 +0900

    Updates

---
+++ precache-manifest.fd2613a1ead690b75fd5944517b9abbd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07c2ac38042b4c63b03c67bdde68551",
+    "url": "/react-map/index.html"
+  },
+  {
+    "revision": "b2ce1e559465f474a995",
+    "url": "/react-map/static/css/main.50a3ed07.chunk.css"
+  },
+  {
+    "revision": "c02f48fa565e60d87ad8",
+    "url": "/react-map/static/js/2.0563cb49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-map/static/js/2.0563cb49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ce1e559465f474a995",
+    "url": "/react-map/static/js/main.5dba9f19.chunk.js"
+  },
+  {
+    "revision": "3f063d4ef913f00bfdce",
+    "url": "/react-map/static/js/runtime-main.d015aa94.js"
+  },
+  {
+    "revision": "77bfab85520a625290f54015e9c91473",
+    "url": "/react-map/static/media/hospital.77bfab85.png"
+  },
+  {
+    "revision": "bd2cf6c1040cafb0014a2d5f5c1a388b",
+    "url": "/react-map/static/media/mask.bd2cf6c1.png"
+  },
+  {
+    "revision": "b8759239cdfd5e152ed3b35f47aa7bf7",
+    "url": "/react-map/static/media/pharmacy.b8759239.png"
+  }
+]);
